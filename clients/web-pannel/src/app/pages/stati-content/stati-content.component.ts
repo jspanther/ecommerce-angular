b975b9b065c13e39a87b40be94e505f19db27641
Mohit-Rathaur <mohit.rathaur@123789.org>
2021-12-20 12:01:47 +0530

    mid codr

--- clients/web-pannel/src/app/pages/stati-content/stati-content.component.ts
+++ clients/web-pannel/src/app/pages/stati-content/stati-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonService } from 'src/app/provider/common.service';
 
 @Component({
   selector: 'app-stati-content',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stati-content.component.css']
 })
 export class StatiContentComponent implements OnInit {
-
-  constructor() { }
+  pageKey:any
+  staticData=[]
+  constructor(private service:CommonService,private router:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.router.queryParams.subscribe(res=>{
+      this.pageKey=res.id
+    })
+
+  }
+  staticContent(id){
+    let url = "static-content/pageKey="+this.pageKey
+    this.service.showSpinner()
+    this.service.getApi('url',0).subscribe(res=>{
+      this.staticData=res['data']
+      this.service
+    })
   }
 
 }
